8aaf444929e9367e91983bdbd6ed94be89277212
= <=>
2023-10-11 18:30:40 -0300

    Obtendo um usuário por id

--- src/main/java/com/sanmartindev/workshopmongo/services/UserService.java
+++ src/main/java/com/sanmartindev/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.sanmartindev.workshopmongo.services;
 
 import com.sanmartindev.workshopmongo.domain.User;
 import com.sanmartindev.workshopmongo.repository.UserRepository;
+import com.sanmartindev.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,15 @@ import java.util.List;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	private UserRepository repo;
-	
+
+    @Autowired
+    private UserRepository repo;
+
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
